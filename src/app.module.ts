50bc248042a9b8b98798e2984cb332e372edf7b8
maaitlunghau <chunhau.py@gmail.com>
2026-02-23 14:55:30 +0700

    feat(config): add Joi validation for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 // root module: use all the sub modules
 
 @Module({
-  imports: [HelloModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        APP_NAME: Joi.string().default('defaultApp')
+      }),
+    }),
+    HelloModule,
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
