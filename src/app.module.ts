ae2b181f1de97ab2445fd1c99b7e6da44909f46c
maaitlunghau <chunhau.py@gmail.com>
2026-02-23 14:25:03 +0700

    chore(app): register UserModule in root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HelloModule } from './hello/hello.module';
+import { UserModule } from './user/user.module';
 
 // root module: use all the sub modules
 
 @Module({
-  imports: [],
+  imports: [HelloModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
