bd38a21109dfb62ccbcf5600152a36a5dfcb7475
maaitlunghau <chunhau.py@gmail.com>
2026-02-23 23:24:24 +0700

    feat(database): configure TypeORM with PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 import Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './posts/entities/post.entity';
 
 // root module: use all the sub modules
 
@@ -19,7 +21,18 @@ import Joi from 'joi';
     }),
     HelloModule,
     UserModule,
-    PostsModule],
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'nestjs_core_fundamentals',
+      entities: [Post],
+      synchronize: true, // only for 'developement' mode
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
