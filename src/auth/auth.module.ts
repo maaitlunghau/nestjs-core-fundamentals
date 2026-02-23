720bc33b3e6b5664523ab77addaa38ff02a36fb3
maaitlunghau <chunhau.py@gmail.com>
2026-02-24 02:20:27 +0700

    feat(auth): configure auth module and controller

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({}),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService]
+})
+export class AuthModule { }
