8dde58b85d4017fc1e813574357c9eaa377f4626
maaitlunghau <chunhau.py@gmail.com>
2026-02-23 23:25:06 +0700

    feat(posts): register Post entity in PostsModule

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
 
 @Module({
+  imports: [
+    // this will make the post repository available for injection
+    // available in the current
+    // scope
+    TypeOrmModule.forFeature([Post]),
+  ],
   controllers: [PostsController],
   providers: [PostsService]
 })
-export class PostsModule {}
+export class PostsModule { }
