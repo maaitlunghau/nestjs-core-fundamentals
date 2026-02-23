b31ae396bf77d414a7e345ae28840f04d197ca8f
maaitlunghau <chunhau.py@gmail.com>
2026-02-23 12:00:13 +0700

    docs: add code comment for entry point and root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// root module: use all the sub modules
+
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// root file: entry point of ur nestjs application
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // global settings
+  // env
+
+  // starts a HTTP server
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
